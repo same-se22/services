7c4366dd5358eefcbd5044788e53d53094277b7a
same-se22 <samesekhanom684@gmail.com>
2021-11-01 15:20:29 +0600

    first commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0th5g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB}:${process.env.PASS}@cluster0.0th5g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -18,7 +18,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    const database = client.db("HolidaysPlanner");
+    const database = client.db("VactionPlanner");
     const OffersCollection = database.collection("Offers");
     const bookCollection = database.collection("BooksOffer");
 
@@ -44,7 +44,7 @@ async function run() {
     });
 
     // books offer
-    app.post("/orders", async (req, res) => {
+    app.post("/oders", async (req, res) => {
       const order = req.body;
       const result = await bookCollection.insertOne(order);
       res.json(result);
@@ -90,15 +90,9 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("running assignment-11 server");
+  res.send("tourism travel server");
 });
 
 app.listen(port, () => {
-  console.log("running assignment-11 server", port);
+  console.log("tourism travel server", port);
 });
-
-
-
-
-
-
