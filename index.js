b5ebe1ba1d3c5ef5050b0194efabedf8f6bef174
same-se22 <samesekhanom684@gmail.com>
2021-11-02 15:44:39 +0600

    first commit

--- index.js
+++ index.js
@@ -9,22 +9,26 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB}:${process.env.PASS}@cluster0.0th5g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m5air.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });;
+console.log(uri)
+// const client = new MongoClient(uri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 async function run() {
   try {
     await client.connect();
+    console.log('database connected')
     const database = client.db("VactionPlanner");
     const OffersCollection = database.collection("Offers");
     const bookCollection = database.collection("BooksOffer");
 
     //   get all offers
     app.get("/offers", async (req, res) => {
-      const offers = await OffersCollection.find({}).toArray();
+      const cursor = OffersCollection.find({})
+      const offers = await cursor.toArray();
+      console.log(cursor)
       res.json(offers);
     });
 
@@ -44,7 +48,7 @@ async function run() {
     });
 
     // books offer
-    app.post("/oders", async (req, res) => {
+    app.post("/orders", async (req, res) => {
       const order = req.body;
       const result = await bookCollection.insertOne(order);
       res.json(result);
@@ -90,9 +94,9 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("tourism travel server");
+  res.send("running vaction planner server");
 });
 
 app.listen(port, () => {
-  console.log("tourism travel server", port);
+  console.log("running vaction planner server", port);
 });
